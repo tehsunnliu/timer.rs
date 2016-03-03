a37ea68d6bbc25dbf49291801600f21d26e1b207
David Rajchenbach-Teller <david.teller@univ-orleans.fr>
2016-03-03 12:37:17 +0100

    Implemented schedule_repeat

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@ extern crate chrono;
 
 use std::cmp::Ordering;
 use std::thread;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering as AtomicOrdering;
 use std::sync::{Arc, Mutex, Condvar};
 use std::sync::mpsc::{channel, Sender};
 use std::collections::BinaryHeap;
-    
 use chrono::{Duration, DateTime, UTC};
 
 /// An item scheduled for delayed execution.
@@ -17,7 +18,14 @@ struct Schedule {
     date: DateTime<UTC>,
 
     /// The callback to execute.
-    cb: Box<FnMut() + Send>
+    cb: Box<FnMut() + Send>,
+
+    /// A mechanism to cancel execution of an item.
+    guard: Guard,
+
+    /// If `Some(d)`, the item must be repeated every interval of
+    /// length `d`, until cancelled.
+    repeat: Option<Duration>
 }
 impl Ord for Schedule {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -106,7 +114,23 @@ impl Scheduler {
                     break;
                 }
                 let mut sched = self.heap.pop().unwrap(); // We just checked that the heap is not empty.
+                if !sched.guard.should_execute() {
+                    // Skip this callback.
+                    continue;
+                }
                 (sched.cb)();
+                if let Some(delta) = sched.repeat {
+                    // This is a repeating timer, so we need to enqueue the next
+                    // call, unless it has been cancelled.
+                    assert!(delay.is_none());
+                    delay = Some(delta);
+                    self.heap.push(Schedule {
+                        date: sched.date + delta,
+                        cb: sched.cb,
+                        guard: sched.guard,
+                        repeat: Some(delta)
+                    });
+                }
             }
 
             match delay {
@@ -200,8 +224,11 @@ impl Timer {
     /// delay. However, it is possible that they will be called a
     /// little after the delay.
     ///
-    /// If the delay is negative, the callback is executed as soon as
-    /// possible.
+    /// If the delay is negative or 0, the callback is executed as
+    /// soon as possible.
+    ///
+    /// This method returns a `Guard` object. If that `Guard` is
+    /// dropped, execution is cancelled.
     ///
     /// # Performance
     ///
@@ -225,7 +252,7 @@ impl Timer {
     /// let timer = timer::Timer::new();
     /// let (tx, rx) = channel();
     ///
-    /// timer.schedule_with_delay(chrono::Duration::seconds(3), move || {
+    /// let _guard = timer.schedule_with_delay(chrono::Duration::seconds(3), move || {
     ///   // This closure is executed on the scheduler thread,
     ///   // so we want to move it away asap.
     ///
@@ -235,7 +262,7 @@ impl Timer {
     /// rx.recv().unwrap();
     /// println!("This code has been executed after 3 seconds");
     /// ```
-    pub fn schedule_with_delay<F>(&self, delay: Duration, cb: F)
+    pub fn schedule_with_delay<F>(&self, delay: Duration, cb: F) -> Guard
         where F: 'static + FnMut() + Send {
         self.schedule_with_date(UTC::now() + delay, cb)
     }
@@ -249,6 +276,10 @@ impl Timer {
     /// If the date is in the past, the callback is executed as soon
     /// as possible.
     ///
+    /// This method returns a `Guard` object. If that `Guard` is
+    /// dropped, execution is cancelled.
+    ///
+    ///
     /// # Performance
     ///
     /// The callback is executed on the Scheduler thread. It should
@@ -260,13 +291,130 @@ impl Timer {
     /// Any failure in `cb` will scheduler thread and progressively
     /// contaminate the Timer and the calling thread itself. You have
     /// been warned.
-    pub fn schedule_with_date<F, T>(&self, date: DateTime<T>, cb: F)
+    pub fn schedule_with_date<F, T>(&self, date: DateTime<T>, cb: F) -> Guard
         where F: 'static + FnMut() + Send, T : chrono::offset::TimeZone
     {
+        self.schedule(date, None, cb)
+    }
+
+    /// Schedule a callback for execution once per interval.
+    ///
+    /// Callbacks are guaranteed to never be called before their
+    /// date. However, it is possible that they will be called a
+    /// little after it.
+    ///
+    /// This method returns a `Guard` object. If that `Guard` is
+    /// dropped, repeat is stopped.
+    ///
+    ///
+    /// # Performance
+    ///
+    /// The callback is executed on the Scheduler thread. It should
+    /// therefore terminate very quickly, or risk causing delaying
+    /// other callbacks.
+    ///
+    /// # Failures
+    ///
+    /// Any failure in `cb` will scheduler thread and progressively
+    /// contaminate the Timer and the calling thread itself. You have
+    /// been warned.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// extern crate timer;
+    /// extern crate chrono;
+    /// use std::thread;
+    /// use std::sync::{Arc, Mutex};
+    ///
+    /// let timer = timer::Timer::new();
+    /// // Number of times the callback has been called.
+    /// let count = Arc::new(Mutex::new(0));
+    ///
+    /// // Start repeating. Each callback increases `count`.
+    /// let guard = {
+    ///   let count = count.clone();
+    ///   timer.schedule_repeating(chrono::Duration::milliseconds(5), move || {
+    ///     *count.lock().unwrap() += 1;
+    ///   })
+    /// };
+    ///
+    /// // Sleep one second. The callback should be called ~200 times.
+    /// thread::sleep(std::time::Duration::new(1, 0));
+    /// let count_result = *count.lock().unwrap();
+    /// assert!(190 <= count_result && count_result <= 210,
+    ///   "The timer was called {} times", count_result);
+    ///
+    /// // Now drop the guard. This should stop the timer.
+    /// drop(guard);
+    ///
+    /// // Let's check that the count stops increasing.
+    /// let count_start = *count.lock().unwrap();
+    /// thread::sleep(std::time::Duration::new(1, 0));
+    /// let count_stop =  *count.lock().unwrap();
+    /// assert_eq!(count_start, count_stop);
+    /// ```
+    pub fn schedule_repeating<F>(&self, repeat: Duration, cb: F) -> Guard
+        where F: 'static + FnMut() + Send
+    {
+        self.schedule(UTC::now() + repeat, Some(repeat), cb)
+    }
+
+    
+    /// Schedule a callback for execution at a given time, then once
+    /// per interval. A typical use case is to execute code once per
+    /// day at 12am.
+    ///
+    /// Callbacks are guaranteed to never be called before their
+    /// date. However, it is possible that they will be called a
+    /// little after it.
+    ///
+    /// This method returns a `Guard` object. If that `Guard` is
+    /// dropped, repeat is stopped.
+    ///
+    ///
+    /// # Performance
+    ///
+    /// The callback is executed on the Scheduler thread. It should
+    /// therefore terminate very quickly, or risk causing delaying
+    /// other callbacks.
+    ///
+    /// # Failures
+    ///
+    /// Any failure in `cb` will scheduler thread and progressively
+    /// contaminate the Timer and the calling thread itself. You have
+    /// been warned.
+    pub fn schedule<F, T>(&self, date: DateTime<T>, repeat: Option<Duration>, cb: F) -> Guard
+        where F: 'static + FnMut() + Send, T : chrono::offset::TimeZone
+    {
+        let guard = Guard::new();
         self.tx.send(Op::Schedule(Schedule {
             date: date.with_timezone(&UTC),
-            cb: Box::new(cb)
+            cb: Box::new(cb),
+            guard: guard.clone(),
+            repeat: repeat
         })).unwrap();
+        guard
+    }
+}
+
+#[derive(Clone)]
+pub struct Guard {
+    should_execute: Arc<AtomicBool>
+}
+impl Guard {
+    fn new() -> Self {
+        Guard {
+            should_execute: Arc::new(AtomicBool::new(true))
+        }
+    }
+    fn should_execute(&self) -> bool {
+        self.should_execute.load(AtomicOrdering::Relaxed)
+    }
+}
+impl Drop for Guard {
+    fn drop(&mut self) {
+        self.should_execute.store(false, AtomicOrdering::Relaxed)
     }
 }
 
@@ -274,6 +422,7 @@ impl Timer {
 fn test_schedule_with_delay() {
     let timer = Timer::new();
     let (tx, rx) = channel();
+    let mut guards = vec![];
 
     // Schedule a number of callbacks in an arbitrary order, make sure
     // that they are executed in the right order.
@@ -282,10 +431,10 @@ fn test_schedule_with_delay() {
     for i in delays.clone() {
         println!("Scheduling for execution in {} seconds", i);
         let tx = tx.clone();
-        timer.schedule_with_delay(Duration::seconds(i), move || {
+        guards.push(timer.schedule_with_delay(Duration::seconds(i), move || {
             println!("Callback {}", i);
             tx.send(i).unwrap();
-        });
+        }));
     }
 
     delays.sort();
@@ -302,12 +451,29 @@ fn test_schedule_with_delay() {
     for i in vec![10, 0] {
         println!("Scheduling for execution in {} seconds", i);
         let tx = tx.clone();
-        timer.schedule_with_delay(Duration::seconds(i), move || {
+        guards.push(timer.schedule_with_delay(Duration::seconds(i), move || {
             println!("Callback {}", i);
             tx.send(i).unwrap();
-        });
+        }));
     }
 
     assert_eq!(rx.recv().unwrap(), 0);
     assert!(UTC::now() - start <= Duration::seconds(1));
+}
+
+#[test]
+fn test_guards() {
+    println!("Testing that callbacks aren't called if the guard is dropped");
+    let timer = Timer::new();
+    let called = Arc::new(Mutex::new(false));
+
+    for i in 0..10 {
+        let called = called.clone();
+        timer.schedule_with_delay(Duration::milliseconds(i), move || {
+            *called.lock().unwrap() = true;
+        });
+    }
+
+    thread::sleep(std::time::Duration::new(1, 0));
+    assert_eq!(*called.lock().unwrap(), false);
 }
